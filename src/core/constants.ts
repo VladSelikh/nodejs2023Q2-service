661da6b9d5b0665c631739762e460485dbc3cc9a
VladSelikh <vladikh2405@gmail.com>
2023-08-01 02:42:21 +0300

    fix: delete unused constants

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -2,5 +2,3 @@ export const USER_NOT_FOUND = 'There is no user with the id specified!';
 export const ARTIST_NOT_FOUND = 'There is no artist with the id specified!';
 export const ALBUM_NOT_FOUND = 'There is no album with the id specified!';
 export const TRACK_NOT_FOUND = 'There is no track with the id specified!';
-export const NOT_VALID_UUID = 'Specified id is not a valid UUID string!';
-export const INVALID_BODY = 'Invalid body format!';
